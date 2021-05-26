a32a7ee7b45c80b3aa7dedc6f49333de940daac7
ahmedhassan202025 <33345323+ahmedhassan202025@users.noreply.github.com>
2021-05-26 16:27:28 +0200

    feat: Add API call

--- frontend_challenge/src/Components/HomeComponent.js
+++ frontend_challenge/src/Components/HomeComponent.js
@@ -1,13 +1,35 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
+const Home = () => {
+  const [repos, setRepos] = useState([]);
+  const [page, setPage] = useState(1);
 
-function Home() {
-  
-    return (
-        <div>
-            <h1>Home</h1>
-        </div>
-      );
-  }
-  export default Home;
-  
+  const getRepos = async (
+    pageNumber = 1,
+    year = "2021",
+    month = "04",
+    day = "26"
+  ) => {
+    const response = await axios.get(
+      `https://api.github.com/search/repositories?q=created:>${year}-${month}-${day}&sort=stars&order=desc&page=${pageNumber}`
+    );
+    console.log(response);
+  };
+
+  useEffect(() => {
+    getRepos();
+  }, []);
+
+  useEffect(() => {
+    getRepos(page);
+  }, [page]);
+
+  return (
+    <div>
+      <h1>Home</h1>
+    </div>
+  );
+};
+export default Home;
